8c962e55d492383b785c65e1448da0beb39887ef
Dillon Reedy <DJR0008@auburn.edu>
2016-10-28 15:34:15 -0400

    The final product of SQL Beautifier
    The finished product of SQL Beautifier, there will be small changes to
    come in the future.

--- SQL_Beautifier/Form1.Designer.cs
+++ SQL_Beautifier/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SQL_Beautifier
 {
-    partial class Form1
+    partial class FormTab
     {
         /// <summary>
         /// Required designer variable.
@@ -28,79 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBoxSQL = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // textBoxSQL
-            // 
-            this.textBoxSQL.Location = new System.Drawing.Point(12, 31);
-            this.textBoxSQL.Multiline = true;
-            this.textBoxSQL.Name = "textBoxSQL";
-            this.textBoxSQL.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxSQL.Size = new System.Drawing.Size(346, 255);
-            this.textBoxSQL.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "SQL Query";
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(128, 296);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(63, 17);
-            this.radioButton1.TabIndex = 2;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Beautify";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(197, 295);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(80, 17);
-            this.radioButton2.TabIndex = 3;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Un-Beautify";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(283, 292);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(375, 326);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBoxSQL);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      this.textBoxSQL = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.button1 = new System.Windows.Forms.Button();
+      this.tabControl1 = new System.Windows.Forms.TabControl();
+      this.Queries = new System.Windows.Forms.TabPage();
+      this.comboBoxChoices = new System.Windows.Forms.ComboBox();
+      this.tabPage2 = new System.Windows.Forms.TabPage();
+      this.listViewKeywords = new System.Windows.Forms.ListView();
+      this.buttonRemoveListviewItem = new System.Windows.Forms.Button();
+      this.buttonAddListviewItem = new System.Windows.Forms.Button();
+      this.textBoxListviewItem = new System.Windows.Forms.TextBox();
+      this.tabControl1.SuspendLayout();
+      this.Queries.SuspendLayout();
+      this.tabPage2.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // textBoxSQL
+      // 
+      this.textBoxSQL.Location = new System.Drawing.Point(6, 23);
+      this.textBoxSQL.Multiline = true;
+      this.textBoxSQL.Name = "textBoxSQL";
+      this.textBoxSQL.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.textBoxSQL.Size = new System.Drawing.Size(346, 255);
+      this.textBoxSQL.TabIndex = 0;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(6, 7);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(59, 13);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "SQL Query";
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(277, 284);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 4;
+      this.button1.Text = "button1";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.buttonExecuteChoice_Click);
+      // 
+      // tabControl1
+      // 
+      this.tabControl1.Controls.Add(this.Queries);
+      this.tabControl1.Controls.Add(this.tabPage2);
+      this.tabControl1.Location = new System.Drawing.Point(12, 12);
+      this.tabControl1.Name = "tabControl1";
+      this.tabControl1.SelectedIndex = 0;
+      this.tabControl1.Size = new System.Drawing.Size(365, 341);
+      this.tabControl1.TabIndex = 5;
+      // 
+      // Queries
+      // 
+      this.Queries.Controls.Add(this.comboBoxChoices);
+      this.Queries.Controls.Add(this.textBoxSQL);
+      this.Queries.Controls.Add(this.button1);
+      this.Queries.Controls.Add(this.label1);
+      this.Queries.Location = new System.Drawing.Point(4, 22);
+      this.Queries.Name = "Queries";
+      this.Queries.Padding = new System.Windows.Forms.Padding(3);
+      this.Queries.Size = new System.Drawing.Size(357, 315);
+      this.Queries.TabIndex = 0;
+      this.Queries.Text = "Query";
+      this.Queries.UseVisualStyleBackColor = true;
+      // 
+      // comboBoxChoices
+      // 
+      this.comboBoxChoices.FormattingEnabled = true;
+      this.comboBoxChoices.Items.AddRange(new object[] {
+            "Beautify",
+            "Commafy",
+            "Quoteify",
+            "Unquoteify"});
+      this.comboBoxChoices.Location = new System.Drawing.Point(150, 284);
+      this.comboBoxChoices.Name = "comboBoxChoices";
+      this.comboBoxChoices.Size = new System.Drawing.Size(121, 21);
+      this.comboBoxChoices.TabIndex = 6;
+      this.comboBoxChoices.SelectedIndexChanged += new System.EventHandler(this.comboBoxChoices_SelectedIndexChanged);
+      // 
+      // tabPage2
+      // 
+      this.tabPage2.Controls.Add(this.textBoxListviewItem);
+      this.tabPage2.Controls.Add(this.buttonAddListviewItem);
+      this.tabPage2.Controls.Add(this.buttonRemoveListviewItem);
+      this.tabPage2.Controls.Add(this.listViewKeywords);
+      this.tabPage2.Location = new System.Drawing.Point(4, 22);
+      this.tabPage2.Name = "tabPage2";
+      this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+      this.tabPage2.Size = new System.Drawing.Size(357, 315);
+      this.tabPage2.TabIndex = 1;
+      this.tabPage2.Text = "SQL Keywords";
+      this.tabPage2.UseVisualStyleBackColor = true;
+      // 
+      // listViewKeywords
+      // 
+      this.listViewKeywords.Location = new System.Drawing.Point(3, 6);
+      this.listViewKeywords.Name = "listViewKeywords";
+      this.listViewKeywords.Size = new System.Drawing.Size(348, 274);
+      this.listViewKeywords.TabIndex = 0;
+      this.listViewKeywords.UseCompatibleStateImageBehavior = false;
+      // 
+      // buttonRemoveListviewItem
+      // 
+      this.buttonRemoveListviewItem.Location = new System.Drawing.Point(276, 286);
+      this.buttonRemoveListviewItem.Name = "buttonRemoveListviewItem";
+      this.buttonRemoveListviewItem.Size = new System.Drawing.Size(75, 23);
+      this.buttonRemoveListviewItem.TabIndex = 1;
+      this.buttonRemoveListviewItem.Text = "Remove";
+      this.buttonRemoveListviewItem.UseVisualStyleBackColor = true;
+      this.buttonRemoveListviewItem.Click += new System.EventHandler(this.buttonRemoveListviewItem_Click);
+      // 
+      // buttonAddListviewItem
+      // 
+      this.buttonAddListviewItem.Location = new System.Drawing.Point(112, 286);
+      this.buttonAddListviewItem.Name = "buttonAddListviewItem";
+      this.buttonAddListviewItem.Size = new System.Drawing.Size(39, 23);
+      this.buttonAddListviewItem.TabIndex = 2;
+      this.buttonAddListviewItem.Text = "Add";
+      this.buttonAddListviewItem.UseVisualStyleBackColor = true;
+      this.buttonAddListviewItem.Click += new System.EventHandler(this.buttonAddListviewItem_Click);
+      // 
+      // textBoxListviewItem
+      // 
+      this.textBoxListviewItem.Location = new System.Drawing.Point(6, 286);
+      this.textBoxListviewItem.Name = "textBoxListviewItem";
+      this.textBoxListviewItem.Size = new System.Drawing.Size(100, 20);
+      this.textBoxListviewItem.TabIndex = 3;
+      // 
+      // FormTab
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(389, 365);
+      this.Controls.Add(this.tabControl1);
+      this.Name = "FormTab";
+      this.Text = "Dillon SQL Beautifier";
+      this.tabControl1.ResumeLayout(false);
+      this.Queries.ResumeLayout(false);
+      this.Queries.PerformLayout();
+      this.tabPage2.ResumeLayout(false);
+      this.tabPage2.PerformLayout();
+      this.ResumeLayout(false);
 
         }
 
@@ -108,9 +180,15 @@
 
         private System.Windows.Forms.TextBox textBoxSQL;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.Button button1;
-    }
+    private System.Windows.Forms.TabControl tabControl1;
+    private System.Windows.Forms.TabPage Queries;
+    private System.Windows.Forms.TabPage tabPage2;
+    private System.Windows.Forms.ListView listViewKeywords;
+    private System.Windows.Forms.ComboBox comboBoxChoices;
+    private System.Windows.Forms.TextBox textBoxListviewItem;
+    private System.Windows.Forms.Button buttonAddListviewItem;
+    private System.Windows.Forms.Button buttonRemoveListviewItem;
+  }
 }
 
--- SQL_Beautifier/Program.cs
+++ SQL_Beautifier/Program.cs
@@ -16,7 +16,7 @@ namespace SQL_Beautifier
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormTab());
         }
     }
 }
